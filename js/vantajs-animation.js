e04dd364f088826720b17d068371aad0937919aa
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2024-09-09 15:19:58 +0700

    Add files via upload

---
+++ js/vantajs-animation.js
@@ -0,0 +1,35 @@
+VANTA.BIRDS({
+  el: "#web",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 605.00,
+  minWidth: 1520.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color1: 0x1c00e3,
+  color2: 0xad00f5
+})
+
+VANTA.GLOBE({
+  el: "#itinfra",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 605.00,
+  minWidth: 1520.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
+
+VANTA.WAVES({
+  el: "#mobile",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 605.00,
+  minWidth: 1520.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1834
+})
